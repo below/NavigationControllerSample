2ec789c734cf483c44c8fd9dbf8e791ef3d68fd5
Alexander von Below <alexander.von_below@telekom.de>
2019-07-09 19:28:25 +0200

    Initial Commit

---
+++ NavigationControllerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationControllerSample
+//
+//  Created by Alexander v. Below on 09.07.19.
+//  Copyright © 2019 Alexander von Below. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
